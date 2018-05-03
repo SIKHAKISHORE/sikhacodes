b7ac01e0f4c70bfab1e471458123a09a56f21d98
HP <HP@DESKTOP-E7C5T0K>
2018-05-03 12:33:10 +0530

    second commit

--- 13thqn/src/com/tcs/Grid.java
+++ 13thqn/src/com/tcs/Grid.java
@@ -1,24 +1,17 @@
 package com.tcs;
-import java.math.BigInteger;
+
 public class Grid {
 
 	public static void main(String[] args) {
-		BigInteger b=BigInteger.valueOf(20);
-		BigInteger output=BigInteger.valueOf(fact(b));
-		//output=fact(20);
-    //output=fact(40)/(fact(2)*fact(38));
-	System.out.println(output);
-	
+		long gridsize =20;
+		long p=1;
+		for(int i=1;i<gridsize;i++)
+		{
+			p*=((2*gridsize)-i)/(i+1);
+		}
+		System.out.println(p);
+		
 	}
-
-
-
-static BigInteger fact(BigInteger x)
-{
-	BigInteger a=BigInteger.valueOf(1);
-	if(x.equals(a))
-		return a;
-	BigInteger product=BigInteger.valueOf((fact(x.subtract(1))).multiply(x));
-	return product;
+	
 }
-}
+	
